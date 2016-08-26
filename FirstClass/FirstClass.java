bbac9d2ea10c5aebf9317e43d76f5bb40ea0041f
ArianaPark <792913@seq.org>
2016-08-25 21:36:08 -0700

    First Commit

---
+++ FirstClass/FirstClass.java
@@ -0,0 +1,26 @@
+public class FirstClass
+{
+    // instance variables - replace the example below with your own
+    private int a;
+    private int b;
+
+    /**
+     * Constructor for objects of class FirstClass
+     */
+    public FirstClass()
+    {
+        // initialise instance variables
+        a = 2;
+        b = 5;
+    }
+
+    //methods
+    public int sum(){
+        return a + b;
+    }
+    
+    public int difference(){
+        return a - b;
+    }
+    
+    }
---
+++ FirstClass/FirstClassRunner.java
@@ -0,0 +1,10 @@
+//Ariana Park
+public class FirstClassRunner
+{
+    public static void main(String[] args){
+        FirstClass fc = new FirstClass();
+        System.out.println("Sum: " + fc.sum());
+        System.out.println("Difference: " + fc.difference());
+    }
+
+}
