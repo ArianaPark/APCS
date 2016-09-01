0e23975b9c84220fd67bb8f8047a438bc263a0ae
ArianaPark <792913@seq.org>
2016-09-01 08:51:20 -0700

    First Commit

---
+++ Project 2 - Ball World/BallRunner.java
@@ -0,0 +1,9 @@
+
+//Ariana Park
+public class BallRunner{
+    public static void main(String[] args){
+        BallWorld bw = new BallWorld(200,200);
+        BallBot bb = new BallBot(bw.BallWorld, TGPoint(0,0), 0, 25);
+        
+    }
+}
