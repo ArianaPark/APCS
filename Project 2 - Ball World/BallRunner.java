c09be5f05150731506ec939ccc78276a1828ff04
ArianaPark <792913@seq.org>
2016-09-07 07:48:23 -0700

    2nd Commit

--- Project 2 - Ball World/BallRunner.java
+++ Project 2 - Ball World/BallRunner.java
@@ -2,8 +2,19 @@
 //Ariana Park
 public class BallRunner{
     public static void main(String[] args){
-        BallWorld bw = new BallWorld(200,200);
-        BallBot bb = new BallBot(bw.BallWorld, TGPoint(0,0), 0, 25);
-        
+        BallWorld ballWorld = new BallWorld(200,200); 
+        TGPoint tgp = new TGPoint(0,0);
+        BallBot ballBot = new BallBot(ballWorld, tgp, 0, 25);
+        boolean a = true;
+        while(a == true){
+            if(ballBot.canMoveForward(BallWorld)){
+                //parameter wrong FIX IT
+                double currentHeading = ballBot.getHeading();
+                ballBot.setHeading( (currentHeading + 90) % 360);
+            }
+            else{
+                ballBot.moveForward();
+            }
+        }
     }
 }
