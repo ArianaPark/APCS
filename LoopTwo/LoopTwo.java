0609cf8afc4abfc7cc38cbba2628113239bbe56d
ArianaPark <792913@seq.org>
2016-09-09 08:52:58 -0700

    1st commit

---
+++ LoopTwo/LoopTwo.java
@@ -0,0 +1,33 @@
+
+/**
+ * Ariana Park
+ * 9/9/16
+ */
+import java.util.Arrays;
+
+public class LoopTwo{
+    //instance variable
+    private int[] list;
+    
+    //constructor, n=length of array
+    public LoopTwo(int n){
+        list = new int[n];
+    }
+    
+    //methods
+    public void loadRandom(){
+        //
+    }
+    
+    public void printArray(){
+        //
+    }
+    
+    public void sortArray(){
+        //
+    }
+    
+    public double findAverage(){
+        //
+    }
+}
