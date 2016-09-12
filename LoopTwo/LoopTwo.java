917fb9a0a3efb8282e993dff63954ccc207f6cb6
ArianaPark <792913@seq.org>
2016-09-12 08:33:54 -0700

    3rd Commit

--- LoopTwo/LoopTwo.java
+++ LoopTwo/LoopTwo.java
@@ -1,4 +1,3 @@
-
 /**
  * Ariana Park
  * 9/9/16
@@ -15,19 +14,34 @@ public class LoopTwo{
     }
     
     //methods
-    public void loadRandom(){
-        //
+    public void loadRandom(){ //what is wrong here?
+        for(int i = 0; i < list.length; i++){
+            int randNum = (int)(Math.random() * list.length) + 1;//RANDOM NOT WORKING
+            list[i] = randNum;
+        }
     }
     
     public void printArray(){
-        //
+        for(int i = 0; i < list.length; i++){
+            if((i+1) % 10 == 0){
+                System.out.println(list[i]);
+            }
+            else{
+                System.out.print(list[i] + "\t");
+            }
+        }
     }
     
     public void sortArray(){
-        //
+        Arrays.sort(list);
     }
     
     public double findAverage(){
-        //
+        double sum = 0.0;
+        for(int i = 0; i < list.length; i++){
+            sum = sum + list[i];
+        }
+        double average = sum/list.length;
+        return average;
     }
 }
--- LoopTwo/LoopTwoRunner.java
+++ LoopTwo/LoopTwoRunner.java
@@ -1,33 +1,16 @@
 
 /**
- * Write a description of class LoopTwoRunner here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Ariana Park
+ * 9/9/16
  */
 public class LoopTwoRunner
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class LoopTwoRunner
-     */
-    public LoopTwoRunner()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public static void main(String[] args){
+        LoopTwo loop2 = new LoopTwo(50);
+        loop2.loadRandom();
+        loop2.printArray();
+        loop2.sortArray();
+        loop2.printArray();
+        System.out.print(loop2.findAverage());
     }
 }
