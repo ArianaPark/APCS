576272a4699853be3716dc832d3ad0e8dd96a37b
ArianaPark <792913@seq.org>
2016-09-13 10:41:38 -0700

    5th commit

--- Project 2 - Ball World/BallRunner.java
+++ Project 2 - Ball World/BallRunner.java
@@ -1,26 +1,28 @@
 
 //Ariana Park
 public class BallRunner{
-    private BallWorld ballWorld; 
-    private TGPoint entrancePoint;
-    private BallBot[] ballBotArray;
+    private static BallWorld ballWorld; 
+    private static TGPoint entrancePoint;
+    private static BallBot[] ballBotArray;
 
     //constructors
     public BallRunner(BallWorld ballWorld, TGPoint entrancePoint, int ballBotArrayLength) {
         ballWorld = ballWorld;
         entrancePoint = entrancePoint;
-        ballBotArray = new BallBot[ballBotArrayLength];
+        ballBotArray = new BallBot[ballBotArrayLength];   
     }
 
     //helper method
-    public int findFreeBallBotIndex(){//problem
+    public static int findFreeBallBotIndex(){
         int returnValue = -1;
         for(int i = 0; i < ballBotArray.length; i++) {
             if(ballBotArray[i] == null)
-                return i;
+                returnValue = i;//problem here?
+            else{
+                returnValue = ballBotArray.length;
+            }
         }
-        if(returnValue == -1)
-            return ballBotArray.length;
+        return returnValue;
     }
 
     //new run method
@@ -31,11 +33,23 @@ public class BallRunner{
         int arrayLength = 10;
         BallRunner ballBotRunner = new BallRunner(ballWorld, tgp, arrayLength);
         while(true){
-            int freeBallBotIndex = ballBotArray.findFreeBallBotIndex();//problem withindex
+            int freeBallBotIndex = findFreeBallBotIndex();
             if(freeBallBotIndex < ballBotArray.length){
                 BallBot ballBot = new BallBot(ballWorld, entrancePoint, (int)(Math.random()*360), 20);
+                ballBotArray[freeBallBotIndex] = ballBot;
+            }
+
+            for(int index = 0; index < ballBotArray.length; index++){
+                if(ballBotArray[index] != null){//should stop from being null
+                    if(ballBotArray[index].canMoveForward(ballWorld)){//object is required, should be ballBot
+                        ballBotArray[index].moveForward();
+                    }
+                    else{
+                        double currentHeading = ballBotArray[index].getHeading();
+                        ballBotArray[index].setHeading( (currentHeading + 65) % 360);
+                    }
+                }
             }
         }
-
     }
 }
