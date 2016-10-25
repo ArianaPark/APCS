05ac9e770bd253db1e926db9e774bb462ee42783
ArianaPark <792913@seq.org>
2016-10-25 08:48:07 -0700

    1st commit

---
+++ Lab1025/ScannerClass.java
@@ -0,0 +1,27 @@
+
+/**
+ * Write a description of class ScannerClass here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+public class ScannerClass
+{
+    //no fields
+    
+    //constructor
+    public ScannerClass(){
+    }
+    
+//     //methods
+//     public void verticalPrint(){
+//         Scanner input = new Scanner();
+//         String ans = "hi";
+//         while(!ans.equals("quit")){
+//             System.out.println("Please input something or \"quit\". ");
+//             ans = input.next();
+//             System.out.println("");
+//         }
+//     }
+}
