ac29021287386c5bbe567041aede766f5cfed8bd
ArianaPark <792913@seq.org>
2016-10-27 08:47:52 -0700

    4th commit

--- Lab1025/ScannerClass.java
+++ Lab1025/ScannerClass.java
@@ -1,4 +1,3 @@
-
 /**
  * Ariana Park
  */
@@ -12,12 +11,34 @@ public class ScannerClass
     }
     
     //method
-    public static void main(String[] args){
+    public static void partOne(){
         Scanner input = new Scanner(System.in);
+        System.out.println("Please input something or \"quit\". ");
         String ans = input.next();
         while(!ans.equals("quit")){
-            System.out.println("Please input something or \"quit\". ");
-            System.out.println("You said: " + ans); //?? not printing anything
+            System.out.println("You said: " + ans + "\nPlease input something or \"quit\". ");
+            ans = input.next();
         }
+    }
+    
+    public static void partTwo(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please input something or \"quit\". ");
+        String ans = input.next();
+        String contain = ans;
+        while(!ans.equals("quit")){
+            if(contain.contains("awesome")){
+                System.out.println("This is an awesome string: " + ans.indexOf("awesome"));
+            }else{
+                System.out.println("This is not an awesome string.");
+            }
+            System.out.println("\nPlease input something or \"quit\". ");
+            ans = input.next();
+        }
+    }
+    
+    public static void main(String[] args){
+        //partOne();
+        partTwo();
     }
 }
