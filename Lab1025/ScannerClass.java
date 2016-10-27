ae13c1050c4559c30af4f8e8d375fa79dbcebffa
ArianaPark <792913@seq.org>
2016-10-26 20:57:48 -0700

    3rd commit

--- Lab1025/ScannerClass.java
+++ Lab1025/ScannerClass.java
@@ -1,12 +1,8 @@
 
 /**
- * Write a description of class ScannerClass here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Ariana Park
  */
 import java.util.Scanner;
-import java.util.regex.MatchResult;
 public class ScannerClass
 {
     //no fields
@@ -19,13 +15,9 @@ public class ScannerClass
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         String ans = input.next();
-        MatchResult mr = input.match();
         while(!ans.equals("quit")){
             System.out.println("Please input something or \"quit\". ");
-            System.out.println("You said: ");
-            for(int i=0; i<mr.groupCount();i++){
-                System.out.println(mr.group(i));
-            }
+            System.out.println("You said: " + ans); //?? not printing anything
         }
     }
 }
